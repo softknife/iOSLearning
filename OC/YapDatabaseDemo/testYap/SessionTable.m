037f19ffeb3411c4f89915648d45d97be60e068b
Eric <hjphfut@163.com>
2017-04-17 14:42:05 +0800

    测试Migration
    测试Yapdatabase中的数据迁移

--- OC/YapDatabaseDemo/testYap/SessionTable.m
+++ OC/YapDatabaseDemo/testYap/SessionTable.m
@@ -13,35 +13,51 @@
 #import <NSObject+MJCoding.h>
 
 @implementation SessionTable
-MJCodingImplementation
-//- (id)initWithCoder:(NSCoder *)decoder // NSCoding deserialization
-//{
-//    if ((self = [super init])) {
-//        _sessionId = [decoder decodeObjectForKey:@"sessionId"];
-//        _sessionName = [decoder decodeObjectForKey:@"sessionName"];
-//        _avatarPath = [decoder decodeObjectForKey:@"avatarPath"];
-//        _lastMsgId = [decoder decodeObjectForKey:@"lastMsgId"];
-//        
-//        _unreadTotal = [decoder decodeIntegerForKey:@"unreadTotal"];
-//        _isDelete = [decoder decodeBoolForKey:@"isDelete"];
-//        _setTopTime = [decoder decodeDoubleForKey:@"setTopTime"];
-//        _isTop = [decoder decodeBoolForKey:@"isTop"];
-//    }
-//    return self;
-//}
+//MJCodingImplementation
+- (id)initWithCoder:(NSCoder *)decoder // NSCoding deserialization
+{
+    if ((self = [super init])) {
+        
+        int32_t version = [decoder decodeInt32ForKey:@"version"];
+        
+        NSLog(@"version=%zd",version);
+        
+        
+        _sessionId = [decoder decodeObjectForKey:@"sessionId"];
+        _sessionName = [decoder decodeObjectForKey:@"sessionName"];
+        _avatarPath = [decoder decodeObjectForKey:@"avatarPath"];
+        _lastMsgId = [decoder decodeObjectForKey:@"lastMsgId"];
+        
+        _unreadTotal = [decoder decodeIntegerForKey:@"unreadTotal"];
+        _isDelete = [decoder decodeBoolForKey:@"isDelete"];
+        _setTopTime = [decoder decodeDoubleForKey:@"setTopTime"];
+        _isTop = [decoder decodeBoolForKey:@"isTop"];
+        
+        // Migration
+        if (version == 1) {
+            // do some thing
+        }else if (version == 2){
+            // do other thing
+            
+        }
+    }
+    return self;
+}
 //
-//- (void)encodeWithCoder:(NSCoder *)encoder // NSCoding serialization
-//{
-//    [encoder encodeObject:_lastMsgId forKey:@"lastMsgId"];
-//    [encoder encodeObject:_avatarPath forKey:@"avatarPath"];
-//    [encoder encodeObject:_sessionName forKey:@"sessionName"];
-//    [encoder encodeObject:_sessionId forKey:@"sessionId"];
-//    
-//    [encoder encodeInteger:_unreadTotal forKey:@"unreadTotal"];
-//    [encoder encodeBool:_isDelete forKey:@"isDelete"];
-//    [encoder encodeDouble:_setTopTime forKey:@"setTopTime"];
-//    [encoder encodeBool:_isTop forKey:@"isTop"];
-//}
+- (void)encodeWithCoder:(NSCoder *)encoder // NSCoding serialization
+{
+    [encoder encodeInt32:2 forKey:@"version"];
+    
+    [encoder encodeObject:_lastMsgId forKey:@"lastMsgId"];
+    [encoder encodeObject:_avatarPath forKey:@"avatarPath"];
+    [encoder encodeObject:_sessionName forKey:@"sessionName"];
+    [encoder encodeObject:_sessionId forKey:@"sessionId"];
+    
+    [encoder encodeInteger:_unreadTotal forKey:@"unreadTotal"];
+    [encoder encodeBool:_isDelete forKey:@"isDelete"];
+    [encoder encodeDouble:_setTopTime forKey:@"setTopTime"];
+    [encoder encodeBool:_isTop forKey:@"isTop"];
+}
 
 
 @end
