b879bcc06e8431ec2d75479901dcbaa5a094e2a2
EricYellow <hjphfut@163.com>
2017-05-17 10:17:11 +0800

    swift-unsafe 和 unowned&weak

---
+++ Swift/Swift3基础/26-UnsafeSwift.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/*:
+ 参考：http://www.swiftyper.com/2017/01/15/unsafe-swift/
+ 
+ */
+var str = "Hello, playground"
---
+++ Swift/Swift3基础/27-unowned&weak.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+/*:
+ http://swift.gg/2017/05/16/unowned-or-weak-lifetime-and-performance/
+ */
+var str = "Hello, playground"
