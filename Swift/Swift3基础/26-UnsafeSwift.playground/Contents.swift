4dbdb24be27125b897278b6382e55753921d306c
EricYellow <hjphfut@163.com>
2017-05-27 09:25:22 +0800

    swift-Raw指针(非类型指针)完善

--- Swift/Swift3基础/26-UnsafeSwift.playground/Contents.swift
+++ Swift/Swift3基础/26-UnsafeSwift.playground/Contents.swift
@@ -3,7 +3,23 @@
 import UIKit
 
 /*:
+ 
  > Memory Layout
+ 
+ Swift语言是用C++写的
+ Swift的核心Library是用Swift自身写的。
+ 对于C++来说，内存区间如下
+ 
+ 堆区
+ 栈区
+ 代码区
+ 全局静态区
+ Swift的内存区间和C++类似。也有存储代码和全局变量的区间，这两种区间比较简单，本文更多专注于以下两个内存区间。
+ 
+ Stack（栈），存储值类型的临时变量，函数调用栈，引用类型的临时变量指针
+ Heap（堆），存储引用类型的实例
+ 
+ 
  - 默认情况下，Swift 是不允许我们直接操纵内存, 所以swift才是类型安全的 ,并且确保所有的变量在使用前都已经被正确地初始化了。
  - 那么,如果我们想要像写C语音一样去直接操纵内存,我们应该怎么做?
     - 官方提供了一系列的unsafe特性,这些就相当于C语言,可以用来直接操纵内存,但是,这种情况下,编译器可以给我们的提示帮助就微乎其微了, 一切都得靠我们自己了!
@@ -184,29 +200,57 @@ let byteCount = stride * count
 do {
     print("Raw pointers test")
     
-    // 申请byteCount大小的内存,对齐方式为alignment
+    // 1.申请byteCount大小的内存,对齐方式为alignment, UnsafeMutableRawPointer，它的名字表明这个指针可以用来读取和存储（改变）原生的字节。
     let pointer = UnsafeMutableRawPointer.allocate(bytes: byteCount, alignedTo: alignment)
     
-    // 保证作用于后,手动释放内存
+    // 2.保证作用于后,手动释放内存
     defer {
         pointer.deallocate(bytes: byteCount, alignedTo: alignment)
     }
     
-    // 存储Int类型的数字42
+    // 3.存储Int类型的数字42
     pointer.storeBytes(of: 42, as: Int.self)
-    // 步进一个步幅后,存储Int类型的数字12
-    pointer.advanced(by: stride).storeBytes(of: 12, as: Int.self)
+    // 4.步进一个步幅后,存储Int类型的数字12(pointer+stride).storeBytes(of: 6, as: Int.self)。
+    pointer.advanced(by: stride).storeBytes(of: 20, as: Int.self)
+    // 因为pointer这个指针类型是 Strideable 的，我们也可以直接使用指针算术运算
+//    (pointer + stride).storeBytes(of: 10, as: Int.self)
+//    pointer.advanced(by: stride).storeBytes(of: 10, as: Int.self)
     
-    // 读取字节
+    
+    
+    // 5.读取字节
     pointer.load(as: Int.self)
     pointer.advanced(by: stride).load(as: Int.self)
+//    (pointer + stride).load(as: Int.self)
+//    pointer.advanced(by: stride).load(as: Int.self)
     
     
-    // 数组指针读取上面的内存数据
+    // 6.数组指针读取上面的内存数据
     let bufferPointer = UnsafeRawBufferPointer(start: pointer, count: byteCount)
     for (index,byte) in bufferPointer.enumerated() {
         print("byte : \(index):\(byte)")
+        // 这里打印了16次,说明bufferPointer遍历是以alignment为单位进行的!
+        
     }
+    
+    /*
+     byte : 0:42
+     byte : 1:0
+     byte : 2:0
+     byte : 3:0
+     byte : 4:0
+     byte : 5:0
+     byte : 6:0
+     byte : 7:0
+         byte : 8:20
+         byte : 9:0
+         byte : 10:0
+         byte : 11:0
+         byte : 12:0
+         byte : 13:0
+         byte : 14:0
+         byte : 15:0
+     */
     
     
 }
