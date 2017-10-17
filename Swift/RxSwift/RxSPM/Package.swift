a568e67026ac6b2c89e6b9b22ba522f3c74dc867
Eric <hjphfut@163.com>
2017-10-17 23:29:00 +0800

    学习RxSwift-获取特定事件

--- Swift/RxSwift/RxSPM/Package.swift
+++ Swift/RxSwift/RxSPM/Package.swift
@@ -6,13 +6,13 @@ import PackageDescription
 let package = Package(
     name: "RxSPM",
     dependencies: [
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "3.0.0"),
-        
-    ],
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .exact("4.0.0-rc.0")),
+        ],
     
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target( name: "RxSPM",dependencies: ["RxSwift"]),
-    ]
+        ]
 )
+
